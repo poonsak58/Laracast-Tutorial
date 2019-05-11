9490888096abb6b7e53cc9c353c0cdae297731ed
Poonsak Aphidetmongkhon <poonsak_aph@freewillsolutions.com>
2019-05-11 15:36:13 +0700

    * Blade Layout Files

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 link : https://laracasts.com/series/laravel-from-scratch-2018
 
 #### EP 3 - Basic Routing
+#### EP 4 - Blade Layout Files
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,16 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-</head>
-<body>
-<h1>Contact Page</h1>
+@extends('layouts.layout')
+@section('title', 'Contact Page')
+@section('content')
 
-<a href="/contact">Contact</a>
-<a href="/about">About</a>
-</body>
-</html>
+    <h1>Contact Page</h1>
+
+@endsection
+
